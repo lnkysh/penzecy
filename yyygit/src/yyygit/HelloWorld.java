93adeb62f10df7818bd8465f1267057a3562b054
lnkysh <69881120+lnkysh@users.noreply.github.com>
2020-08-19 16:40:22 +0800

    Update HelloWorld.java
    555666

--- yyygit/src/yyygit/HelloWorld.java
+++ yyygit/src/yyygit/HelloWorld.java
@@ -8,7 +8,8 @@ System.out.println("nihao");
 System.out.println("nihao2");
 System.out.println("nihao3");
 System.out.println("nihao4");
-
+System.out.println("nihao3");
+System.out.println("nihao4");
 	}
 
 }
