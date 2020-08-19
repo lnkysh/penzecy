dea37e0feb1b463ed4b4bb1429892a4898556e4f
lnkysh <69881120+lnkysh@users.noreply.github.com>
2020-08-19 16:56:12 +0800

    Update HelloWorld.java
    来了

--- yyygit/src/yyygit/HelloWorld.java
+++ yyygit/src/yyygit/HelloWorld.java
@@ -7,12 +7,7 @@ public class HelloWorld {
 System.out.println("nihao");
 System.out.println("nihao2");
 System.out.println("nihao3");
-System.out.println("nihao4");
 
-System.out.println("777");
-
-System.out.println("nihao3");
-System.out.println("nihao4");
 
 	}
 
